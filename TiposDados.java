b7a5fffbc8953713bcad34973d29dd09b96062b1
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2021-10-21 18:21:07 -0300

    arquivos da api

---
+++ TiposDados.java
@@ -0,0 +1,16 @@
+
+package jhonatastomaz.bot.api.database.jhonatas;
+
+/**
+ * @author JH�NATAS
+ *
+ */
+public enum TiposDados {
+	STRING,
+	INTEGER,
+	DOUBLE,
+	SHORT,
+	LONG,
+	FLOAT,
+	BOOLEAN
+}
