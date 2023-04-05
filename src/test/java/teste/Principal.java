010c45d5513f737e0042a0b872f5476b56992ad7
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2023-04-05 17:03:45 -0300

    Update Principal.java

--- src/test/java/teste/Principal.java
+++ src/test/java/teste/Principal.java
@@ -17,7 +17,7 @@ public class Principal {
 	public static void main(String[] args) {
 	
 	
-	//	System.out.println(col);
+		System.out.println("");
 		
 		//ConnectionManager.getConnection();
 	   MysqlConnection connection = new MysqlConnection();
