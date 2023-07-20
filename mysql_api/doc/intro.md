f8cf86e2675704efe7e1b895d3bca42854249dba
Jhonatas48 <34196298+Jhonatas48@users.noreply.github.com>
2023-07-19 22:24:07 -0300

    revert

---
+++ mysql_api/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mysql_api
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject mysql_api "0.0.2.0"
+  :description "A very simple api mysql."
+  :url "https://github.com/Jhonatas48/mysql_api_simples"
+  :dependencies [ [org.clojure/clojure "1.10.3"] ])
